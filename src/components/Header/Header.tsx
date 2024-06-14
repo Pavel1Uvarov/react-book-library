124d282f71d578cb2df6db38eaa70752155eda6b
unknown <pavel2uvarov@gmail.com>
2024-06-14 18:57:19 +0200

    feat(Project): add ui components && header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className="bg-white border-b py-2 flex items-center justify-center shadow-md">
+      <img src={Logo} alt="Logo book library" className="w-32" />
+    </header>
+  );
+};
+
+export default Header;
